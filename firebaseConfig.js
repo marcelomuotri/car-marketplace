2b32748d12eb5aa19a9c24aab32dc9908145946f
Marcelo <muotrimarcelo@gmail.com>
2024-07-11 12:41:19 +0200

    testing .env

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -17,6 +17,18 @@ import {
 // import {...} from "firebase/storage";
 
 // Your web app's Firebase configuration
+console.log('API Key:', import.meta.env.VITE_FIREBASE_API_KEY)
+console.log('Auth Domain:', import.meta.env.VITE_FIREBASE_AUTH_DOMAIN)
+console.log('Database URL:', import.meta.env.VITE_FIREBASE_DATABASE_URL)
+console.log('Project ID:', import.meta.env.VITE_FIREBASE_PROJECT_ID)
+console.log('Storage Bucket:', import.meta.env.VITE_FIREBASE_STORAGE_BUCKET)
+console.log(
+  'Messaging Sender ID:',
+  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID
+)
+console.log('App ID:', import.meta.env.VITE_FIREBASE_APP_ID)
+console.log('Measurement ID:', import.meta.env.VITE_FIREBASE_MEASUREMENT_ID)
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
