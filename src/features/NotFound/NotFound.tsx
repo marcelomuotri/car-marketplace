53cb6bfc3d3369ae74314c68456c31b536cc6672
Marcelo <muotrimarcelo@gmail.com>
2024-09-25 16:28:25 +0200

    minor fix

--- src/features/NotFound/NotFound.tsx
+++ src/features/NotFound/NotFound.tsx
@@ -1,25 +1,27 @@
 import boyfishing from '../../assets/images/boyFishing.png'
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Fade } from '@mui/material'
 import FButton from '../../components/FButton/FButton'
 import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
   const navigate = useNavigate()
   return (
-    <Box
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginTop: 40,
-      }}
-    >
-      <Typography style={{ textAlign: 'center', fontSize: 22 }}>
-        Algo salió mal
-      </Typography>
-      <img src={boyfishing} style={{ width: 300, marginTop: -10 }} />
-      <FButton title='Volver' onClick={() => navigate('home')} />
-    </Box>
+    <Fade in={true} timeout={800}>
+      <Box
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: 40,
+        }}
+      >
+        <Typography style={{ textAlign: 'center', fontSize: 22 }}>
+          Algo salió mal
+        </Typography>
+        <img src={boyfishing} style={{ width: 300, marginTop: -10 }} />
+        <FButton title='Volver' onClick={() => navigate('home')} />
+      </Box>
+    </Fade>
   )
 }
 
